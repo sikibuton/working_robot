7fdff9d7c5158689fb7b26a900cb699e5a8b9637
koki <sikibuton@outlook.com>
2013-09-12 16:20:30 +0900

    サーボテスト用に変更1500us出すように設定

--- servo_test.py
+++ servo_test.py
@@ -11,10 +11,21 @@
 
 import os
 import serial
+import array
 
 def main():
     os.system("echo ttyO1_armhf.com > /sys/devices/bone_capemgr.9/slots")
-    ser = serial.Serial("/dev/ttyO1",baudrate =9600)
+    ser = serial.Serial("/dev/ttyO1",baudrate = 9600)
+    terget = 1500 #unit is us
+    channel = 0
+    data = terget*4
+    send_list = [0x84]
+    send_list.append(channel)
+    send_list.append(data & 0x7f)
+    send_list.append((data>>7)&0x7f)
+    ser.write(array.array('B',send_list))
+
+
     ser.write("hello")
 
 if __name__ == '__main__':
